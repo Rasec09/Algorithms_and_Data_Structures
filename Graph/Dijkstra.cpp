9bb7f70c3da0f04c7fddd439367f370fafc41199
Rasec09 <malcom_hades@hotmail.com>
2017-09-06 01:34:39 -0500

    Create Dijkstra.cpp

---
+++ Graph/Dijkstra.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1000000000;
+
+vector<vii> Ady;
+vi dist;
+
+void dijkstra(int s) {
+
+	dist[s] = 0;
+	priority_queue<ii, vector<ii>, greater<ii> > pq;
+	pq.push(ii(0, s));
+
+	while (!pq.empty()) {
+		ii front = pq.top();
+		pq.pop();
+		int d = front.first, u = front.second;
+		if (d > dist[u]) continue;
+		for (auto v : Ady[u]) {
+			if (dist[u] + v.second < dist[v.first]) {
+				dist[v.first] = dist[u] + v.second;
+				pq.push(ii(dist[v.first], v.first));
+			}
+		}
+	}
+}
+
+int main() {
+	
+	int V, E, a, b, w;
+
+	scanf("%d %d", &V, &E);
+	Ady.assign(V, vii());
+	dist.assign(V, INF);
+	for (int i = 0; i < E; i++) {
+		scanf("%d %d %d", &a, &b, &w);
+		Ady[--a].push_back(ii(--b, w));
+	}
+	
+	dijkstra(0);
+	for (int i = 1; i < V; i++)
+		printf("%d ", dist[i]);
+	return 0;
+}
