3934770c7f4624947322300109720e646ab009b6
Rasec09 <malcom_hades@hotmail.com>
2017-09-06 01:28:55 -0500

    Create Bellman-Ford.cpp

---
+++ Graph/Bellman-Ford.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1000000000;
+
+vector<vii> Ady;
+vi dist;
+
+bool BellmanFord(int s, int V) {
+	
+	dist[s] = 0;
+	for (int i = 0; i < V - 1; i++) {
+		for (int u = 0; u < V; u++) {
+			for (auto v : Ady[u])
+				dist[v.first] = min(dist[v.first], dist[u] + v.second);
+		}
+	}
+
+	bool cicloNegativo = false;
+	for (int u = 0; u < V; u++) {
+		for (auto v : Ady[u])
+			if (dist[v.first] > dist[u] + v.second)
+				cicloNegativo = true;			
+	}
+
+	return cicloNegativo;
+}
+
+int main() {
+	
+	int TC, V, E, a, b, w;
+
+	scanf("%d", &TC);
+	while (TC--) {
+		scanf("%d %d", &V, &E);
+		Ady.assign(V, vii());
+		dist.assign(V, INF);
+		for (int i = 0; i < E; i++) {
+			scanf("%d %d %d", &a, &b, &w);
+			Ady[a].push_back(ii(b, w));
+		}
+		printf( BellmanFord(0, V) ? "possible\n" : "not possible\n" );
+	}
+	return 0;
+}
