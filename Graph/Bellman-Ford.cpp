6c5f2ea4026f91170e528a18e110f20bd166ed93
Rasec09 <malcom_hades@hotmail.com>
2019-07-28 22:24:31 -0500

    Update Bellman-Ford.cpp

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int, int> ii;
+typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
-const int INF = 1000000000;
+const int INF = 1e9;
 
 vector<vii> Ady;
 vi dist;
 
 bool BellmanFord(int s, int V) {
-	
 	dist[s] = 0;
 	for (int i = 0; i < V - 1; i++) {
 		for (int u = 0; u < V; u++) {
 			for (auto v : Ady[u])
-				dist[v.first] = min(dist[v.first], dist[u] + v.second);
+				if (dist[u] < INF)
+					dist[v.first] = min(dist[v.first], dist[u] + v.second);
 		}
 	}
-
 	bool cicloNegativo = false;
 	for (int u = 0; u < V; u++) {
 		for (auto v : Ady[u])
 			if (dist[v.first] > dist[u] + v.second)
 				cicloNegativo = true;			
 	}
-
 	return cicloNegativo;
 }
 
