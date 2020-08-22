60e0054affd96a3779dabfed0b1479aaf4074f7a
Rasec09 <malcom_hades@hotmail.com>
2020-08-22 01:21:18 -0500

    Update SuffixArray.cpp

--- Strings/SuffixArray.cpp
+++ Strings/SuffixArray.cpp
@@ -29,14 +29,18 @@ struct SuffixArray {
 	//lcp(i, j) = min(lcp[i + 1], lcp[i + 2], ..., lcp[j])
 	vector<int> buildLCP() {
 	    int n = s.size();
-	    vector<int> lcp(n);
-	    for (int i = 0, k = 0; i < n - 1; i++) {
-		int j = sa[rank[i] - 1];
-		while (s[i + k] == s[j + k]) k++;
-		lcp[rank[i]] = k;
-		if (k) k--;
-	    }
-	    return lcp;
+        vector<int> lcp(n - 1, 0); //lcp[i] = longest common prefix between prefix i and i - 1 
+        for (int i = 0, k = 0; i < n; i++) {
+            if (rank[i] < n - 1) {
+                int j = sa[rank[i] + 1];
+                while (i + k < n && j + k < n && s[i + k] == s[j + k])
+                    k++;
+                lcp[rank[i]] = k;
+                if (k)
+                	k--;
+            }
+        }
+        return lcp;
 	}
 };
 
