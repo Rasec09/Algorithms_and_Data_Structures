d7ecdcabd0d6c1797b777625e7928edd6ed1875b
Rasec09 <malcom_hades@hotmail.com>
2021-06-10 20:58:17 -0500

    Update Order_statistic_set.cpp

--- Data Structures/Order_statistic_set.cpp
+++ Data Structures/Order_statistic_set.cpp
@@ -5,7 +5,8 @@ using namespace std;
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
 using namespace __gnu_pbds;
- 
+
+//Use less<int> for unique elements
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
  
 int main() {
